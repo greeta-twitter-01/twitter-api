ff6a98d4aebec327681899695953504df71b2fa9
skyglass <skyglass2001@gmail.com>
2023-11-19 06:39:10 +0100

    prepare for deployment

---
+++ docker-app-restart.sh
@@ -0,0 +1,31 @@
+docker-compose down
+
+mvn clean install -DskipTests
+
+cd ./elastic-query-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=elastic-query-service
+
+cd ../analytics-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=analytics-service
+
+cd ../kafka-streams-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=kafka-streams-service
+
+cd ../twitter-to-kafka-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=twitter-to-kafka-service
+
+cd ../kafka-to-elastic-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=kafka-to-elastic-service
+
+cd ../gateway-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=gateway-service
+
+cd ../
+
+docker-compose up -d
